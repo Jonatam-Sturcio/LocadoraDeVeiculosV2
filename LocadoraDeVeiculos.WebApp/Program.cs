7a5d4f9434644b14b5449f77eb4275c13a4fd8c9
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 19:26:38 -0300

    MVC Taxa

--- LocadoraDeVeiculos.WebApp/Program.cs
+++ LocadoraDeVeiculos.WebApp/Program.cs
@@ -1,12 +1,15 @@
 using LocadoraDeVeiculos.Aplicacao.ModuloPlanoCobranca;
+using LocadoraDeVeiculos.Aplicacao.ModuloTaxa;
 using LocadoraDeVeiculos.Aplicacao.ModuloVeiculo;
 using LocadoraDeVeiculos.Aplicacao.Servicos;
 using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
 using LocadoraDeVeiculos.Dominio.ModuloPlanoCobranca;
+using LocadoraDeVeiculos.Dominio.ModuloTaxa;
 using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
 using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
 using LocadoraDeVeiculos.Infra.Orm.ModuloGrupoVeiculos;
 using LocadoraDeVeiculos.Infra.Orm.ModuloPlanoCobranca;
+using LocadoraDeVeiculos.Infra.Orm.ModuloTaxa;
 using LocadoraDeVeiculos.Infra.Orm.ModuloVeiculo;
 using LocadoraDeVeiculos.WebApp.Mapping.Resolvers;
 using System.Reflection;
@@ -15,47 +18,50 @@ namespace LocadoraDeVeiculos.WebApp;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddDbContext<LocadoraDbContext>();
+		builder.Services.AddDbContext<LocadoraDbContext>();
 
-        builder.Services.AddScoped<IRepositorioGrupoVeiculos, RepositorioGrupoVeiculosEmOrm>();
-        builder.Services.AddScoped<IRepositorioVeiculo, RepositorioVeiculoEmOrm>();
-        builder.Services.AddScoped<IRepositorioPlanoCobranca, RepositorioPlanoCobrancaEmOrm>();
+		builder.Services.AddScoped<IRepositorioGrupoVeiculos, RepositorioGrupoVeiculosEmOrm>();
+		builder.Services.AddScoped<IRepositorioVeiculo, RepositorioVeiculoEmOrm>();
+		builder.Services.AddScoped<IRepositorioPlanoCobranca, RepositorioPlanoCobrancaEmOrm>();
+		builder.Services.AddScoped<IRepositorioTaxa, RepositorioTaxaEmOrm>();
 
-        builder.Services.AddScoped<ServicoGrupoVeiculos>();
-        builder.Services.AddScoped<ServicoVeiculo>();
-        builder.Services.AddScoped<ServicoPlanoCobranca>();
+		builder.Services.AddScoped<ServicoGrupoVeiculos>();
+		builder.Services.AddScoped<ServicoVeiculo>();
+		builder.Services.AddScoped<ServicoPlanoCobranca>();
+		builder.Services.AddScoped<ServicoTaxa>();
 
-        builder.Services.AddScoped<FotoValueResolver>();
+		builder.Services.AddScoped<FotoValueResolver>();
+		builder.Services.AddScoped<GrupoVeiculosValueResolver>();
 
-        builder.Services.AddAutoMapper(cfg =>
-        {
-            cfg.AddMaps(Assembly.GetExecutingAssembly());
-        });
+		builder.Services.AddAutoMapper(cfg =>
+		{
+			cfg.AddMaps(Assembly.GetExecutingAssembly());
+		});
 
-        builder.Services.AddControllersWithViews();
+		builder.Services.AddControllersWithViews();
 
-        var app = builder.Build();
+		var app = builder.Build();
 
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseHsts();
-        }
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseHsts();
+		}
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
 
-        app.UseRouting();
+		app.UseRouting();
 
-        app.UseAuthorization();
+		app.UseAuthorization();
 
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+		app.MapControllerRoute(
+			name: "default",
+			pattern: "{controller=Home}/{action=Index}/{id?}");
 
-        app.Run();
-    }
+		app.Run();
+	}
 }
---
+++ LocadoraDeVeiculos.WebApp/Views/Taxa/Detalhes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.Taxa
+{
+    public class DetalhesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
