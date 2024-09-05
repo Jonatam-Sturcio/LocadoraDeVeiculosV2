ea32cec8f60cdfc5c720a5cc3a40a15e0a6103ad
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 20:49:02 -0300

    Dominio + Teste Unidade Cliente

---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/IRepositorioCliente.cs
@@ -0,0 +1,6 @@
+﻿using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente;
+public interface IRepositorioCliente : IRepositorio<Cliente>
+{
+}
---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/TipoCadastroClienteEnum.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente;
+public enum TipoCadastroClienteEnum
+{
+	[Display(Name = "Pessoa Física")] CPF,
+	[Display(Name = "Pessoa Jurídica")] CNPJ
+}
