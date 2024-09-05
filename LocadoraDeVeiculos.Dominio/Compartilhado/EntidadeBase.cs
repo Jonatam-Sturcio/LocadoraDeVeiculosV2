d208768079a32aae2d0913b674d2defd15549a4a
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 10:55:21 -0300

    Dominio GrupoVeiculos

--- LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
+++ LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
@@ -1,5 +1,7 @@
 ﻿namespace LocadoraDeVeiculos.Infra.Orm.Compartilhado;
-public class EntidadeBase
+public abstract class EntidadeBase
 {
     public int Id { get; set; }
+
+    public abstract List<string> Validar();
 }
---
+++ LocadoraDeVeiculos.Dominio/ModuloGrupoVeiculos/GrupoVeiculos.cs
@@ -0,0 +1,23 @@
+﻿using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+public class GrupoVeiculos : EntidadeBase
+{
+    public string Nome { get; set; }
+
+    protected GrupoVeiculos() { }
+    public GrupoVeiculos(string nome)
+    {
+        Nome = nome;
+    }
+
+    public override List<string> Validar()
+    {
+        List<string> erros = new List<string>();
+
+        if (Nome.Length < 3)
+            erros.Add("O nome é obrigatório");
+
+        return erros;
+    }
+}
