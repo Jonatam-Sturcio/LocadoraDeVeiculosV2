ef2b5754a897d32c2b18bb104d8005bcea92817b
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 11:01:34 -0300

    Teste unitário GrupoVeiculo

---
+++ LocadoraDeVeiculos.Teste.Unidade/ModuloGrupoVeiculos/GrupoVeiculosTests.cs
@@ -0,0 +1,29 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+
+namespace LocadoraDeVeiculos.Teste.Unidade.ModuloGrupoVeiculos;
+[TestClass]
+[TestCategory("Unidade")]
+public class GrupoVeiculosTests
+{
+    [TestMethod]
+    public void Deve_Criar_Instancia_Valida()
+    {
+        var grupo = new GrupoVeiculos("SUV");
+
+        var erros = grupo.Validar();
+
+        Assert.AreEqual(0, erros.Count);
+    }
+
+    [TestMethod]
+    public void Deve_Criar_Instancia_Com_Erro()
+    {
+        var grupo = new GrupoVeiculos("");
+
+        var erros = grupo.Validar();
+
+        List<string> errosEsperados = ["O nome é obrigatório"];
+        Assert.AreEqual(1, erros.Count);
+        CollectionAssert.AreEqual(errosEsperados, erros);
+    }
+}
