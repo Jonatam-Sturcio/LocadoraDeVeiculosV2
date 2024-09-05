6d409afac0f8de69e8de8ae59911d9d916b4da9a
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 18:26:24 -0300

    MVC PlanoCobranca

---
+++ LocadoraDeVeiculos.WebApp/Views/PlanoCobranca/Listar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.PlanoCobranca
+{
+    public class ListarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.WebApp/Views/PlanoCobranca/_Informacoes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.PlanoCobranca
+{
+    public class _InformacoesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
