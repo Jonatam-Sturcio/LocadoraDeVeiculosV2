d352ff6383621715436284a0fc9217d4c89e60a9
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 18:49:41 -0300

    Resolver GrupoVeiculos

--- LocadoraDeVeiculos.WebApp/Mapping/VeiculoProfile.cs
+++ LocadoraDeVeiculos.WebApp/Mapping/VeiculoProfile.cs
@@ -5,9 +5,9 @@ using LocadoraDeVeiculos.WebApp.Models;
 
 namespace LocadoraDeVeiculos.WebApp.Mapping;
 
-public class AutomovelProfile : Profile
+public class VeiculoProfile : Profile
 {
-    public AutomovelProfile()
+    public VeiculoProfile()
     {
         CreateMap<InserirVeiculoViewModel, Veiculo>()
             .ForMember(dest => dest.Foto, opt => opt.MapFrom<FotoValueResolver>());
@@ -23,9 +23,9 @@ public class AutomovelProfile : Profile
 
         CreateMap<Veiculo, DetalhesVeiculoViewModel>()
             .ForMember(dest => dest.GrupoVeiculos, opt => opt.MapFrom(src => src.GrupoVeiculos!.Nome));
-        /*
+
         CreateMap<Veiculo, EditarVeiculoViewModel>()
             .ForMember(v => v.Foto, opt => opt.Ignore())
-            .ForMember(v => v.GruposVeiculos, opt => opt.MapFrom<GrupoVeiculosValueResolver>());*/
+            .ForMember(v => v.GruposVeiculos, opt => opt.MapFrom<GrupoVeiculosValueResolver>());
     }
 }
