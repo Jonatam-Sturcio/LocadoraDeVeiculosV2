13b52b203d6b7478d227b8d8fed5304617d4913d
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-06 16:15:01 -0300

    Dominio + Infra Funcionario

---
+++ LocadoraDeVeiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloAutenticacao;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+public class Funcionario : EntidadeBase
+{
+    public int UsuarioId { get; set; }
+    public int EmpresaId { get; set; }
+    public Usuario? Empresa { get; set; }
+    public string NomeCompleto { get; set; }
+    public string Email { get; set; }
+    public DateTime Admissao { get; set; }
+    public decimal Salario { get; set; }
+
+    protected Funcionario() { }
+
+    public Funcionario(string nomeCompleto, string email, DateTime admissao, decimal salario, int empresaId)
+    {
+        NomeCompleto = nomeCompleto;
+        Admissao = admissao;
+        Salario = salario;
+        Email = email;
+        EmpresaId = empresaId;
+    }
+
+    public override List<string> Validar()
+    {
+        return [];
+    }
+}
