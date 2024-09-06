955eaf6b7bbdb0a7936403d2955eb357d927b864
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 21:24:24 -0300

    MVC Cliente

---
+++ LocadoraDeVeiculos.WebApp/Views/Cliente/Inserir.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.Cliente
+{
+    public class InserirModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LocadoraDeVeiculos.WebApp/Views/Cliente/Listar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.Cliente
+{
+    public class ListarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
