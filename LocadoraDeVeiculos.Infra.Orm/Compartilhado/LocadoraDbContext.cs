b034ae6657860ff043a0fedacb86f60c5c161495
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 21:06:46 -0300

    Infra + Test Integração Cliente

--- LocadoraDeVeiculos.Infra.Orm/Compartilhado/LocadoraDbContext.cs
+++ LocadoraDeVeiculos.Infra.Orm/Compartilhado/LocadoraDbContext.cs
@@ -1,4 +1,5 @@
-﻿using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+﻿using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
 using LocadoraDeVeiculos.Dominio.ModuloPlanoCobranca;
 using LocadoraDeVeiculos.Dominio.ModuloTaxa;
 using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
@@ -8,29 +9,30 @@ using Microsoft.Extensions.Configuration;
 namespace LocadoraDeVeiculos.Infra.Orm.Compartilhado;
 public class LocadoraDbContext : DbContext
 {
-    public DbSet<GrupoVeiculos> GruposVeiculos { get; set; }
-    public DbSet<Veiculo> Veiculos { get; set; }
-    public DbSet<PlanoCobranca> PlanosCobranca { get; set; }
-    public DbSet<Taxa> Taxas { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
+	public DbSet<GrupoVeiculos> GruposVeiculos { get; set; }
+	public DbSet<Veiculo> Veiculos { get; set; }
+	public DbSet<PlanoCobranca> PlanosCobranca { get; set; }
+	public DbSet<Taxa> Taxas { get; set; }
+	public DbSet<Cliente> Clientes { get; set; }
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		var config = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
 
-        var connectionString = config.GetConnectionString("SqlServer");
+		var connectionString = config.GetConnectionString("SqlServer");
 
-        optionsBuilder.UseSqlServer(connectionString);
+		optionsBuilder.UseSqlServer(connectionString);
 
-        base.OnConfiguring(optionsBuilder);
-    }
+		base.OnConfiguring(optionsBuilder);
+	}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        var assembly = typeof(LocadoraDbContext).Assembly;
-        modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		var assembly = typeof(LocadoraDbContext).Assembly;
+		modelBuilder.ApplyConfigurationsFromAssembly(assembly);
 
-        base.OnModelCreating(modelBuilder);
-    }
+		base.OnModelCreating(modelBuilder);
+	}
 }
