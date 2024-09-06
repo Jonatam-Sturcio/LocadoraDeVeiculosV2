b3c7e3ee2031a8ea1d1a25cbe9481799243364d7
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-06 17:01:00 -0300

    MVC Funcionario

---
+++ LocadoraDeVeiculos.WebApp/Views/Funcionario/Inserir.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.Funcionario
+{
+    public class InserirModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
