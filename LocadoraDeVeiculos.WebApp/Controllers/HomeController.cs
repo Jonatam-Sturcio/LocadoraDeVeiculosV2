32f9aa263fcd04ded09924df26604c99894a04dc
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-06 20:02:25 -0300

    Refatoração na NavBar

--- LocadoraDeVeiculos.WebApp/Controllers/HomeController.cs
+++ LocadoraDeVeiculos.WebApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LocadoraDeVeiculos.WebApp.Controllers;
+[Authorize(Roles = "Empresa,Funcionario")]
 public class HomeController : Controller
 {
     public IActionResult Index()
