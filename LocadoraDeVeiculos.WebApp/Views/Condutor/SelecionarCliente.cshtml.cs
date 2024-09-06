e04ea23b4e83312f4d003de5fabbb5b9264d4fd4
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-09-05 22:38:27 -0300

    MVC Condutor

---
+++ LocadoraDeVeiculos.WebApp/Views/Condutor/SelecionarCliente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LocadoraDeVeiculos.WebApp.Views.Condutor
+{
+    public class SelecionarClienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
